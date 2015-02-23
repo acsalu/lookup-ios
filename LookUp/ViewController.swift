39e3110bd2b7772ca5489f6296848835e4c91c63
Acsa Lu <acsa0210@gmail.com>
2015-02-23 12:57:50 +0800

    Initiate the project

---
+++ LookUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LookUp
+//
+//  Created by Acsa Lu on 2/23/15.
+//  Copyright (c) 2015 Acsa Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
